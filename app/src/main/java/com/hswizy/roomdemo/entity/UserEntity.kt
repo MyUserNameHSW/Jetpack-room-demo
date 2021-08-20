119d21d10cf0830ce0e3380aeaf835f0e7456809
gongzhe <zhe.gong@ebonex.pro>
2021-08-20 14:36:58 +0800

    fix null

--- app/src/main/java/com/hswizy/roomdemo/entity/UserEntity.kt
+++ app/src/main/java/com/hswizy/roomdemo/entity/UserEntity.kt
@@ -9,13 +9,10 @@ import androidx.room.PrimaryKey
  * @date: 2021/8/19
  * @desc:
  */
-//自定义表名，如果没自定义就使用类名作为表名
 @Entity(tableName = "table_user")
 data class UserEntity(
-    //设置主键，id自增
     @PrimaryKey(autoGenerate = true)
     val id: Long? = null,
-    //自定义字段名称何默认值，不自定则使用变量名称
     @ColumnInfo(name = "user_name", defaultValue = "new_user")
     var userName: String? = null,
 
